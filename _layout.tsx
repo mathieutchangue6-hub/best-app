2f367d7bbbd85a064b3a3b58b92d5309cd0324e1
mathieutchangue6-hub <mathieutchangue6@gmail.com>
2026-04-11 22:19:46 +0100

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -1,7 +1,4 @@
 import { Stack } from 'expo-router';
-import { useEffect } from 'react';
-import * as Notifications from 'expo-notifications';
-
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
