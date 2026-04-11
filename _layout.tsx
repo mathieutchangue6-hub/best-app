842134682a07962a57b5824531fdc64994acd26c
mathieutchangue6-hub <mathieutchangue6@gmail.com>
2026-04-11 22:05:28 +0100

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -2,14 +2,6 @@ import { Stack } from 'expo-router';
 import { useEffect } from 'react';
 import * as Notifications from 'expo-notifications';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: false,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
-
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
