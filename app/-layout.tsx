8effc2a93a330cab3e6d44161323c26b74a8fea0
mathieutchangue6-hub <mathieutchangue6@gmail.com>
2026-04-13 11:23:58 +0100

    Create -layout.tsx

---
+++ app/-layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="member" />
+      <Stack.Screen name="admin" />
+    </Stack>
+  );
+}
